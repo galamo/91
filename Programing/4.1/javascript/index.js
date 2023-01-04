e06923472f87e14cf07a700bc18cdd4d01a09d3c
zeev mindali <mindalizeev@gmail.com>
2023-01-04 21:26:25 +0200

    js1

---
+++ Programing/4.1/javascript/index.js
@@ -0,0 +1,18 @@
+console.log("Hello JS is running");
+
+var country = "England";
+var city = "London";
+var numberOfCitizens = 9000000;
+var numberOfCitizensString = "9M";
+
+console.log(country, city, numberOfCitizensString);
+console.log("The country is: " + country + " And the City is " + city);
+
+// 3 digits number
+var number = 966;
+console.log("The number user input " + number);
+// divide with 100
+var divided = number / 100;
+var integerNumber = parseInt(divided);
+var result = integerNumber;
+console.log("The hundrad digit is " + result);
