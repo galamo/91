1742037e2e8f122b6efd86d018559b5102f9339b
zeev mindali <mindalizeev@gmail.com>
2023-01-08 19:13:48 +0200

    hw

---
+++ Programing/8.1/page15/9/index.js
@@ -0,0 +1,12 @@
+var numBetween10And99 = 63;
+
+var ahadot = numBetween10And99 % 10;
+var asarot = parseInt(numBetween10And99 / 10);
+var newAsarot = ahadot * 10;
+var newAhadot = asarot * 1;
+var opositeNumber = newAsarot + newAhadot;
+console.log("From " + numBetween10And99 + " To " + opositeNumber);
+
+console.log(
+  (numBetween10And99 % 10) * 10 + parseInt(numBetween10And99 / 10) * 1
+);
