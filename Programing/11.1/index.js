67eb982cc5b7a7c9b87884a3f3114ce058706707
zeev mindali <mindalizeev@gmail.com>
2023-01-11 21:01:45 +0200

    a

--- Programing/11.1/index.js
+++ Programing/11.1/index.js
@@ -36,9 +36,57 @@ var email2 = user2 + gmailDomain;
 console.log(email2);
 
 // declare a function & implementation
-function convertUserToEmail() {
-  console.log("Start convertUserToEmail");
-  console.log("End convertUserToEmail");
+function convertUserToEmail(un, d, isInternal) {
+  var com = ".com";
+  var coil = ".co.il";
+  if (isInternal == true) {
+    return un + "@" + d + ".b" + coil;
+  } else {
+    return un + "@" + d + ".b" + com;
+  }
 }
+
 // call function ( execution )
-convertUserToEmail();
+var result = convertUserToEmail("gal", "hotmail", true);
+console.log("the new email is: " + result);
+
+console.log(convertUserToEmail("gal", "hotmail", true));
+console.log(convertUserToEmail("lior", "gmail", false));
+console.log(convertUserToEmail("noy", "icloud", true));
+console.log(convertUserToEmail("tomer", "walla", false));
+
+function printMyName() {
+  var myName = "Gal Amouyal";
+  console.log(myName);
+  return myName;
+}
+
+printMyName();
+printMyName();
+
+var resultFromPrintMyName = printMyName();
+console.log("Function result: " + resultFromPrintMyName);
+
+function getCarPost(company, year, model, km, hand, price, phone) {
+  return (
+    "Car info: Company " +
+    company +
+    ", Year: " +
+    year +
+    " , Price:" +
+    price +
+    " , phone:" +
+    phone
+  );
+}
+
+function getApartmentPost(city, numberOfRooms, price) {
+  return `The city apartment is ${city} and the number of rooms is: ${numberOfRooms}, price ${price} `;
+}
+
+var post1 = getCarPost("Nissan", 2014, "Qashqai", 77000, 1, 65000, 050538169);
+console.log(post1);
+var post2 = getCarPost("BMW", 2022, "x5", 100, 1, 650000, 0505381691);
+console.log(post2);
+var post3 = getApartmentPost("Ashdod", 5, "2.2m");
+console.log(post3);
