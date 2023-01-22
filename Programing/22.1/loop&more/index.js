c782a6a323fc1ae6a924ef179813dfd2800a16d2
zeev mindali <mindalizeev@gmail.com>
2023-01-22 20:00:03 +0200

    a

--- Programing/22.1/loop&more/index.js
+++ Programing/22.1/loop&more/index.js
@@ -16,3 +16,22 @@ function cart() {
   console.log(products);
 }
 cart();
+
+function movies() {
+  var nom = parseInt(prompt("Please enter the number of movies"));
+
+  // declare array in js (empty)
+  // var fullOfShit = ["pasta", "pizza", "fl", "coffee", "", null, undefined, 0, true];
+  // var p = Array();
+  var movies = [];
+  while (nom > 0) {
+    // DO WHAT EVER YOU WANT
+    var currentMovie = prompt("Please Enter the name of the movie!?");
+    movies.push(currentMovie);
+    // nop = nop - 1
+    nom--;
+  }
+
+  console.log(movies);
+}
+movies();
