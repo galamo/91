bced36bbf093b5552fbefbcc0c25c9b7831ac139
galamouyal <galamouyal88@gmail.com>
2023-02-08 18:50:36 +0200

    a

--- Programing/8.2/zuma/index.js
+++ Programing/8.2/zuma/index.js
@@ -1,7 +1,15 @@
 function init() {
-
+  const order = new Order(
+    "galamouyal88@gmail.com",
+    5,
+    2542546246,
+    ["peanuts"],
+    "i would like to seat next to Tobi from Tuba-Zangaria",
+    new Date().toString(),
+    new Date().toString(),
+    true
+  );
+  console.log(order);
 }
 
 init();
-
-// please confirm when its ready...............
