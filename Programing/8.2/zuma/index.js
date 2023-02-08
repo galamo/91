1c95f5a703636894fd75d28715b99df01391c7fa
galamouyal <galamouyal88@gmail.com>
2023-02-08 21:09:17 +0200

    a

--- Programing/8.2/zuma/index.js
+++ Programing/8.2/zuma/index.js
@@ -17,9 +17,7 @@ function init() {
 
   const clearTableButton = document.querySelector("#clearTable");
   clearTableButton.addEventListener("click", clearTableFn);
-  function clearTableFn() {
-    DOM.ordersTableBody.innerHTML = "";
-  }
+
   function addNewOrderFn(event) {
     // console.log(event); // event
     // console.log(this); // button!
@@ -28,8 +26,49 @@ function init() {
     console.log(DOM.numberOfSeats.value);
     orders.push(new Order(DOM.emailInput.value, DOM.numberOfSeats.value));
 
-    // draw(orders);
+    draw(orders);
+    clearForm();
   }
 }
-console.log("Index is loaded!");
+function clearForm() {
+  DOM.emailInput.value = "";
+  DOM.numberOfSeats.value = "";
+}
+function clearTableFn() {
+  DOM.ordersTableBody.innerHTML = "";
+}
+function draw(ordersArray) {
+  if (Array.isArray(ordersArray) === false) return;
+  // document.createElement!
+  // DOM.ordersTableBody.append
+  // DOM.ordersTableBody > tr > td,td,td,td
+  clearTableFn();
+  for (let index = 0; index < ordersArray.length; index++) {
+    const currentOrder = ordersArray[index];
+    // create row
+    const tableRow = document.createElement("tr");
+    // create orderNumber column
+    const tdOrderId = document.createElement("td");
+    tdOrderId.innerText = currentOrder.orderNumber;
+    // create email column
+    const tdEmail = document.createElement("td");
+    tdEmail.innerText = currentOrder.email;
+    // create numberOfSeats column
+    const tdNumberOfSeats = document.createElement("td");
+    tdNumberOfSeats.innerText = currentOrder.numberOfSeats;
+    // create Button column
+    const tdActions = document.createElement("td");
+    const buttonDelete = document.createElement("button");
+    buttonDelete.classList.add("btn", "btn-danger");
+    buttonDelete.innerText = "X";
+    tdActions.append(buttonDelete);
+    buttonDelete.addEventListener("click", function () {
+      orders.splice(index, 1);
+      draw(orders);
+    });
+
+    tableRow.append(tdOrderId, tdEmail, tdNumberOfSeats, tdActions);
+    DOM.ordersTableBody.append(tableRow);
+  }
+}
 init();
