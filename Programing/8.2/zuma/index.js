285928d17278344f9b16c8b1e0a3f377e5e32f98
galamouyal <galamouyal88@gmail.com>
2023-02-08 20:22:58 +0200

    a

--- Programing/8.2/zuma/index.js
+++ Programing/8.2/zuma/index.js
@@ -1,7 +1,7 @@
 const DOM = {
   emailInput: null,
   numberOfSeats: null,
-  //   allergins: null,
+  ordersTableBody: null,
 };
 
 let orders = [];
@@ -9,11 +9,17 @@ let orders = [];
 function init() {
   DOM.emailInput = document.querySelector("#orderEmail");
   DOM.numberOfSeats = document.querySelector("#numberOfSeats");
+  DOM.ordersTableBody = document.querySelector("#ordersTable tbody");
   //   making onclick="addNNewOrder()" from JS:
   //   const addNewOrderButton = document.getElementById("addNewOrderButton");
   const addNewOrderButton = document.querySelector("#addNewOrderButton");
   addNewOrderButton.addEventListener("click", addNewOrderFn);
 
+  const clearTableButton = document.querySelector("#clearTable");
+  clearTableButton.addEventListener("click", clearTableFn);
+  function clearTableFn() {
+    DOM.ordersTableBody.innerHTML = "";
+  }
   function addNewOrderFn(event) {
     // console.log(event); // event
     // console.log(this); // button!
@@ -21,6 +27,8 @@ function init() {
     console.log(DOM.emailInput.value);
     console.log(DOM.numberOfSeats.value);
     orders.push(new Order(DOM.emailInput.value, DOM.numberOfSeats.value));
+
+    // draw(orders);
   }
 }
 console.log("Index is loaded!");
