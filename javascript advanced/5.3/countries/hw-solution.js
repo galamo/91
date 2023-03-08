035c9f7f7065055ecc28d3b2e743f4fd9518b9d5
galamouyal <galamouyal88@gmail.com>
2023-03-08 19:01:10 +0200

    a

--- javascript advanced/5.3/countries/hw-solution.js
+++ javascript advanced/5.3/countries/hw-solution.js
@@ -1,11 +1,10 @@
 // 3. implement function that return all the countries according the currency sent, if the currency is used in the country return the country code / name
 
-function getCountriesByCurrency(currency) {
+function getCountriesByCurrencyCode(currency) {
   // we are searching here by 3Code currency => ILS
   let countriesResult = [];
   for (let index = 0; index < countries.length; index++) {
     const currentCountry = countries[index];
-    console.log(JSON.stringify(currentCountry?.currencies));
     if (currentCountry?.currencies && currentCountry?.currencies[currency]) {
       countriesResult.push(currentCountry);
     }
@@ -15,13 +14,31 @@ function getCountriesByCurrency(currency) {
   });
 }
 
-console.log(getCountriesByCurrency("USD"));
+console.log(getCountriesByCurrencyCode("USD"));
 
+function getCountriesByCurrencyName(currencyName) {
+  // we are searching here by 3Code currency => ILS
+  let countriesResult = [];
+  for (let index = 0; index < countries.length; index++) {
+    const currentCountry = countries[index];
+    if (currentCountry?.currencies) {
+      const cArray = Object.values(currentCountry?.currencies);
+      const isCurrencyExist = cArray.some((currency) =>
+        currency?.name?.includes(currencyName)
+      );
+      if (isCurrencyExist) {
+        countriesResult.push(currentCountry);
+      }
+    }
+  }
+  return countriesResult.map((c) => {
+    return { name: c?.name?.common, population: c.population };
+  });
+}
 
-// print all the keys & values from flags key 
-// Objects.<QUESTION> ? 
-// options:
-// keys
-// values
-// entries 
- 
+console.log(getCountriesByCurrencyName("d"));
+
+// print all the keys & values from flags
+// ??????????????????????????????????????
+// ??????????????????????????????????????
+// ??????????????????????????????????????
