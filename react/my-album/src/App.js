1e5aeb1ee6eb7994e2f29ad63f3e7aa3a41e5031
zeev mindali <mindalizeev@gmail.com>
2023-05-07 21:40:51 +0300

    album

--- react/my-album/src/App.js
+++ react/my-album/src/App.js
@@ -4,6 +4,31 @@ import AlbumCard from "./components/AlbumCard";
 import "./container.css";
 import css from "./app.module.css";
 import AlbumEvent from "./components/AlbumEvent";
+const dummyDataAlbums = [
+  {
+    imageUrl:
+      "https://www.thatbangkoklife.com/wp-content/uploads/2020/03/koh-nang-yuan.jpg",
+    description: "Fun in thailand",
+    date: new Date().toLocaleDateString(),
+  },
+  {
+    imageUrl:
+      "https://media.cnn.com/api/v1/images/stellar/prod/200924183413-dubai-9-1.jpg?q=w_1600,h_900,x_0,y_0,c_fill/w_1280",
+    description: "Fun in Dubai",
+    date: new Date().toLocaleDateString(),
+  },
+  {
+    imageUrl: "https://www.bardugo.com/wp-content/uploads/11.jpg",
+    description: "Fun in Israel",
+    date: new Date().toLocaleDateString(),
+  },
+  {
+    imageUrl: "https://media.timeout.com/images/105434111/image.jpg",
+    description: "Fun in TLV",
+    date: new Date().toLocaleDateString(),
+  },
+];
+
 const dummyDataEvents = [
   {
     eventName: "Avi's Pessah",
@@ -63,41 +88,15 @@ function App() {
         })}
       </div>
       <div className="albumContainer">
-        <AlbumCard
-          imageUrl="https://www.thatbangkoklife.com/wp-content/uploads/2020/03/koh-nang-yuan.jpg"
-          description="fun in thai"
-          date={"'5/7/2020, 6:53:07 PM'"}
-        />
-        <AlbumCard
-          imageUrl="https://www.thatbangkoklife.com/wp-content/uploads/2020/03/koh-nang-yuan.jpg"
-          description="fun in thai"
-          date={"'5/7/2020, 6:53:07 PM'"}
-        />
-        <AlbumCard
-          imageUrl="https://www.thatbangkoklife.com/wp-content/uploads/2020/03/koh-nang-yuan.jpg"
-          description="fun in thai"
-          date={"'5/7/2020, 6:53:07 PM'"}
-        />
-        <AlbumCard
-          imageUrl="https://www.thatbangkoklife.com/wp-content/uploads/2020/03/koh-nang-yuan.jpg"
-          description="fun in thai"
-          date={"'5/7/2020, 6:53:07 PM'"}
-        />
-        <AlbumCard
-          imageUrl="https://www.thatbangkoklife.com/wp-content/uploads/2020/03/koh-nang-yuan.jpg"
-          description="fun in thai"
-          date={"'5/7/2020, 6:53:07 PM'"}
-        />
-        <AlbumCard
-          imageUrl="https://www.thatbangkoklife.com/wp-content/uploads/2020/03/koh-nang-yuan.jpg"
-          description="fun in thai"
-          date={"'5/7/2020, 6:53:07 PM'"}
-        />
-        <AlbumCard
-          imageUrl="https://www.thatbangkoklife.com/wp-content/uploads/2020/03/koh-nang-yuan.jpg"
-          description="fun in thai"
-          date={"'5/7/2020, 6:53:07 PM'"}
-        />
+        {dummyDataAlbums.map((albumCard) => {
+          return (
+            <AlbumCard
+              imageUrl={albumCard.imageUrl}
+              description={albumCard.description}
+              date={albumCard.date}
+            />
+          );
+        })}
       </div>
     </div>
   );
